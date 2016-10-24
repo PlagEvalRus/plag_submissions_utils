8a2b90d2e1f2486765d4e70f965930b38e56706e
dvzubarev <dvzubarev@29efa7aa-725c-44cf-bb9f-e177210b73cd>
2016-10-24 21:38:24 +0000

    q
    git-svn-id: http://ids.isa.ru:8082/exactus/modules/plag_submissions_checker/trunk@13640 29efa7aa-725c-44cf-bb9f-e177210b73cd

--- tools/run_checker.sh
+++ tools/run_checker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 subm_dir="$1"
-# ./checker_cli.py -v -i examples/$subm_dir/sources_list.xlsx -s examples/$subm_dir/sources/
-./bin/submission_checker   -a /home/denin/Yandex.Disk/workspace/sci/plag/corpora/our_plag_corp/submissions/$subm_dir/*.rar
+base_path="/home/denin/Yandex.Disk/workspace/sci/plag/corpora/our_plag_corp"
+dir="submissions"
+shopt -s extglob
+./bin/submission_checker -a $base_path/$dir/$subm_dir/*@(.rar|.zip)
 
