36c8906fe940289cb5dc07e81c8258bea5cb358b
denin <dvzubarev@yandex.ru>
2017-03-14 13:41:52 +0300

    skip hidden and temporary files

--- plag_submissions_utils/common/extract_utils.py
+++ plag_submissions_utils/common/extract_utils.py
@@ -9,6 +9,12 @@ import pyunpack as arc
 class InvalidSubmission(Exception):
     pass
 
+def _skip_file(filename):
+    if filename.startswith(".") or \
+       filename.startswith("~"):
+        return True
+    return False
+
 def extract_submission(arch_path, dest_dir):
     """raise InvalidSubmission if submission is malformed"""
     arc.Archive(arch_path, backend="patool").extractall(dest_dir)
@@ -16,11 +22,14 @@ def extract_submission(arch_path, dest_dir):
     sources_list_file = ""
 
     for dirpath, dirnames, filenames in os.walk(dest_dir):
-        for dirname in dirnames:
+        if fs.basename(dirpath) == "__MACOSX":
+            continue
+
+        for dirname in [d for d in dirnames if not _skip_file(d)]:
             if dirname.lower().find("sources") != -1:
                 sources_dir = fs.join(dirpath, dirname)
 
-        for filename in filenames:
+        for filename in [f for f in filenames if not _skip_file(f)]:
             if filename.lower().find("sources_list") != -1:
                 sources_list_file = fs.join(dirpath, filename)
                 break
