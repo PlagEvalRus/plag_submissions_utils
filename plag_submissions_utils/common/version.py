bdb6ad4ef3e03d9c0235590fd3f424cf7427434f
bakarov <amirbakarov@gmail.com>
2017-12-21 09:41:52 +0000

    update version checl

--- plag_submissions_utils/common/version.py
+++ plag_submissions_utils/common/version.py
@@ -8,7 +8,9 @@ def determine_version_by_id(susp_id):
         return "0"
     elif 3 < susp_id_int < 2000:
         return "1"
-    elif 2000 <= susp_id_int < 4000:
+    elif 2000 <= susp_id_int < 3000:
         return "2"
+    elif 3000 <= susp_id_int < 4000:
+        return "3"
     else:
         raise RuntimeError("Invalid susp_id: %s" % susp_id)
